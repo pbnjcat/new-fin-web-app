28cdeeccf9f0a10f9aa8ac57b83f26954a1e1fbe
pbnjcat <morphlingblade88@gmail.com>
2024-06-16 20:12:31 -0700

    initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
